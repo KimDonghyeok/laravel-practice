dd663024761062a97e7dfe5338c60b2e8f3a45db
KimDonghyeok <qoophon@gmail.com>
2023-01-20 16:48:53 +0900

    FEAT Chirp write & store  in db

---
+++ resources/js/Pages/Chirp/Index.vue
@@ -0,0 +1,30 @@
+<script setup>
+import InputError from '@/Components/InputError.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { useForm } from '@inertiajs/vue3';
+
+
+
+const form = useForm({
+    message: "",
+})
+</script>
+
+<template>
+
+    <Head title="Chirps" />
+
+    <AuthenticatedLayout>
+        <div class="max-w-4xl mx-auto p-4 sm:p-6 lg:p-8">
+            <form @submit.prevent="form.post(route('chirps.store'), { onSuccess: () => form.reset() })">
+                <textarea class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                          v-model="form.message"
+                          placeholder="What's on your mind?"></textarea>
+                <InputError class="mt-2"
+                            :message="form.errors.message" />
+                <PrimaryButton class="mt-4">Chirp</PrimaryButton>
+            </form>
+        </div>
+    </AuthenticatedLayout>
+</template>
