658972784c560cb077548d3fbfc53b16a325e095
KimDonghyeok <qoophon@gmail.com>
2023-01-25 18:09:43 +0900

    MODIFY use SoftDeletes to Chirp model

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Chirp extends Model
 {
     use HasFactory;
+    use SoftDeletes;    // not actually delete row from the DB, add "deleted_at" column to indicating time and date that which model was "deleted"
 
     protected $fillable = [
         'message',
